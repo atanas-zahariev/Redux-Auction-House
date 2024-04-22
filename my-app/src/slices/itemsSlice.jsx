d58f2e74c81d4ed6688c36790f12dcdfacbf2629
Verhus <atanaszaharieff@abv.bg>
2024-04-22 15:42:29 +0300

    Add user field to itemSlice.

--- my-app/src/slices/itemsSlice.jsx
+++ my-app/src/slices/itemsSlice.jsx
@@ -10,7 +10,8 @@ export const getItems = createAsyncThunk(
     'items/fetchItems',
     async (_, { rejectWithValue }) => {
         try {
-            const items = getAllDataInSystem();
+            const items = await getAllDataInSystem();
+            console.log(items);
             return items;
         } catch (error) {
             rejectWithValue(error);
@@ -20,21 +21,35 @@ export const getItems = createAsyncThunk(
 
 const initialState = itemsAdapter.getInitialState({
     status: 'idle',
-    error: null
+    error: null,
+    user: null
 });
 
 const itemsSlice = createSlice({
     name: 'items',
     initialState,
     reducers: {
-
+        setUserToCatalog(state, action) {
+            action.payload.type = 'user'
+            state.user = makeCorrectIdForRedux(action.payload);
+        },
+        clearUserFromCatalog(state, action) {
+            state.user = null;
+        }
     },
 
     extraReducers: (builder) => {
         builder
             .addCase(getItems.fulfilled, (state, action) => {
                 state.status = 'fetchItemsSucceeded';
-                itemsAdapter.addMany(state, action.payload.items.map(makeCorrectIdForRedux));
+                itemsAdapter.addMany(state, action.payload.items.map(item => {
+                    item.type = 'item'
+                    return makeCorrectIdForRedux(item)
+                }));
+                if (action.payload.user) {
+                    action.payload.user.type = 'user';
+                    state.user = makeCorrectIdForRedux(action.payload.user);
+                }
             })
             .addCase(getItems.rejected, (state, action) => {
                 state.status = 'fetchItemsFaild';
@@ -46,6 +61,8 @@ const itemsSlice = createSlice({
 });
 
 export default itemsSlice.reducer;
+
+export const { setUserToCatalog, clearUserFromCatalog } = itemsSlice.actions;
 
 export const { selectAll: selectItems, selectById: selectItemById } = itemsAdapter.getSelectors(state => state.items);
 

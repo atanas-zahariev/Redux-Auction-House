16cfffd3edcb271dea3bd5cee2b192571ef5724a
Verhus <atanaszaharieff@abv.bg>
2024-04-22 15:52:20 +0300

    Get user from itemsSlice.

--- my-app/src/components/common/details/DetailsComponent.jsx
+++ my-app/src/components/common/details/DetailsComponent.jsx
@@ -6,36 +6,18 @@ import Spinner from '../Spinner';
 import NotOwner from './NotOwnerComponent';
 import Owner from './OwnerComponent';
 
-import { selectItemById } from '../../../slices/itemsSlice';
+import { selectItemById, selectUserFromCatalog } from '../../../slices/itemsSlice';
 import { api } from '../../../services/dataService';
 import { useEffect, useState } from 'react';
 
 export default function Details() {
     const { id } = useParams();
-    let item = useSelector(state => selectItemById(state, id));
+    const item = useSelector(state => selectItemById(state, id));
+    const user = useSelector(state => selectUserFromCatalog(state));
+    console.log(user);
+    console.log(item);
 
-    // console.log(item);
 
-    const {getSpecificDataWithId} = api();
-    
-    const [item1,setItem] = useState({});
-
-    const getItem = async () => {
-        try {
-            const result = await getSpecificDataWithId(id);
-            setItem(item => ({...item,...result}));
-        } catch (error) {
-            // getError(error);
-        }
-    };
-
-    useEffect(() => {
-        // cleanError();
-        getItem();
-        // eslint-disable-next-line
-    }, []);
-
-     console.log(item1);
     // if (item.item) {
     //     const isOwner = item.item.owner === item.user?._id;
     //     if (isOwner) {

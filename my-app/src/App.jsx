7507dcd000c943d6df71b7929d485aa13e8433a2
atanas-zahariev <atanaszaharieff@abv.bg>
2024-04-24 22:44:57 +0300

    Add Edit.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,6 +9,7 @@ import Register from './auth/RegisterComponent';
 import Catalog from './components/common/catalog/CatalogComponent';
 import Details from './components/common/details/DetailsComponent';
 import UserClosedOffers from './components/common/closed-offers/UserClosedOffersComponent';
+import Edit from './components/action/EditItemComponent';
 
 
 
@@ -24,6 +25,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/catalog' element={<Catalog />} />
           <Route path='/details/:id' element={<Details />} />
+          <Route path='/edit/:id' element={<Edit />} />
           <Route path='/login' element={<Login />} />
           <Route path='/logout' element={<Logout />} />
           <Route path='/register' element={<Register />} />

c290d4980b131ff718184c289046c2242432d20b
atanas-zahariev <atanaszaharieff@abv.bg>
2024-04-28 08:47:15 +0300

    Initializing Notifications.

---
+++ server/models/Notifications.js
@@ -0,0 +1,20 @@
+const { Schema, model, Types } = require('mongoose');
+
+
+const notificationsSchema = new Schema({
+    incomingMessage: {
+        type: String,
+        required: true,
+        minLength: [3, 'Your message must be at least 3 characters.'],
+        maxLength: [200, 'Your message must be at most 200 characters.']
+    },
+
+    product: { type: Types.ObjectId, ref: 'Item', required: true },
+
+    bider: { type: Types.ObjectId, ref: 'User', required: true }
+
+});
+
+const Notifications = model('Notifications', notificationsSchema);
+
+module.exports = Notifications;

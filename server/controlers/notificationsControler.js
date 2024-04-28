aca77b89e2eb6bebb24b284511d705c649d228c6
atanas-zahariev <atanaszaharieff@abv.bg>
2024-04-28 11:01:21 +0300

    Add post request for createNotification.

--- server/controlers/notificationsControler.js
+++ server/controlers/notificationsControler.js
@@ -1,3 +1,6 @@
+const { hasUser } = require('./guard');
+const errorParser = require('../utyl/parser');
+
 const {
     getAllNotifications,
     getNotificationById,
@@ -7,6 +10,24 @@ const {
 } = require('../services/notificationService');
 
 const notificationsControler = require('express').Router()
+
+notificationsControler.post('/createNot',hasUser(), async (req, res) => {
+    const { message, product, user } = req.body;
+
+    const item = {
+        message,
+        product,
+        user: req.user._id
+    }
+
+    try {
+        const result = await createNotification(item)
+        return result;
+    } catch (error) {
+        const message = errorParser(error);
+        res.status(400).json(message)
+    }
+})
 
 
 module.exports = notificationsControler;

74fc9d9077e1d65035ce36dba1c8694d2103adb4
atanas-zahariev <atanaszaharieff@abv.bg>
2024-04-28 10:37:09 +0300

    Add notificationsControler.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,9 @@
 const authControler = require("../controlers/authControler");
 const itemControler = require("../controlers/itemControler");
+const notificationsControler = require("../controlers/notificationsControler");
 
 module.exports = (app) => {
     app.use('/auth', authControler);
     app.use('/house', itemControler);
+    app.use('/notification', notificationsControler);
 };

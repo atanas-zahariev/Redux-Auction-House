8f43d4b88691d430573c01519ef07546595c4db3
atanas-zahariev <atanaszaharieff@abv.bg>
2024-05-03 17:51:12 +0300

    add fetchItemsBeforeRender to get initial data.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -11,24 +11,29 @@ import './index.css';
 import { store } from './app/store';
 import { getItems } from './slices/itemsSlice';
 
-store.dispatch(getItems());
-
-const container = document.getElementById('root');
-
-if (container) {
-  const root = createRoot(container);
-
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </React.StrictMode>,
-  );
-} else {
-  throw new Error(
-    'Root element with ID \'root\' was not found in the document. Ensure there is a corresponding HTML element with the ID \'root\' in your HTML file.',
-  );
+async function fetchItemBeforeRender(){
+  
+  await store.dispatch(getItems());
+  
+  const container = document.getElementById('root');
+  
+  if (container) {
+    const root = createRoot(container);
+  
+    root.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </React.StrictMode>,
+    );
+  } else {
+    throw new Error(
+      'Root element with ID \'root\' was not found in the document. Ensure there is a corresponding HTML element with the ID \'root\' in your HTML file.',
+    );
+  }
 }
+
+fetchItemBeforeRender();

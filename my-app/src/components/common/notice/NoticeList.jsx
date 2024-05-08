36ef4e574dd224b431a88974fbaae13616fdadea
atanas-zahariev <atanaszaharieff@abv.bg>
2024-05-08 11:41:04 +0300

    Filter notices for user notice.

--- my-app/src/components/common/notice/NoticeList.jsx
+++ my-app/src/components/common/notice/NoticeList.jsx
@@ -2,33 +2,39 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { selectNotices } from '../../../slices/notificationsSlice';
+import { selectUserFromCatalog } from '../../../slices/itemsSlice';
 
 export default function NoticeList() {
     const notices = useSelector(state => selectNotices(state));
-    
-    console.log(notices);
+    const currentuser = useSelector(state => selectUserFromCatalog(state));
 
-
+    const userNotice = notices.filter(notice => notice.product.owner === currentuser.id);
+    console.log(userNotice);
     return (
         <section id="catalog-section">
 
             <h1>Yours Notifications</h1>
 
             <div className="noticeList">
-                <ul className="list">
-                    {notices.map(notice =>
-                        <li key={notice.id} className="partialNotice">
-                            <div className="f-right">
-                                <Link to={`/notice/${notice.id}`} className="action pad-small f-left">See details</Link>
-                            </div>
-                            <p className="message">From {notice.user.firstname} for {notice.product.title}: {notice.message}</p>
-                        </li>)}
-                </ul>
+                {userNotice?.length > 0 ?
+                    <ul className="list">
+                        {userNotice.map(notice =>
+                            <li key={notice.id} className="partialNotice">
+                                <div className="f-right">
+                                    <Link to={`/notice/${notice.id}`} className="action pad-small f-left">See details</Link>
+                                </div>
+                                <p className="message">From: {notice.user.firstname} {notice.user.lastname}. For - {notice.product.title}: {notice.message}</p>
+                            </li>)}
+                    </ul> :
+                    <div className="item pad-large align-center">
+                        <p>You have no notifications!</p>
+                    </div>
+                }
             </div>
 
             <div className="item padded">
                 <footer>
-                    <div>Listed by Gosho </div>
+                    <div>Listed by {currentuser.firstname} </div>
                 </footer>
 
             </div>

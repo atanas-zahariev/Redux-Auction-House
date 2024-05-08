eda6fc78079fc59f4dfd38ea9bac75e164a6cc89
atanas-zahariev <atanaszaharieff@abv.bg>
2024-05-08 15:09:30 +0300

    Add div to html structure.

--- my-app/src/components/common/notice/RecivedNotice.jsx
+++ my-app/src/components/common/notice/RecivedNotice.jsx
@@ -5,48 +5,48 @@ import { selectNoticeById } from '../../../slices/notificationsSlice';
 import { selectUserFromCatalog } from '../../../slices/itemsSlice';
 
 export default function RecivedNotices() {
-    const {id} = useParams();
+    const { id } = useParams();
 
-    const notice = useSelector(state => selectNoticeById(state,id));
+    const notice = useSelector(state => selectNoticeById(state, id));
     const currentuser = useSelector(state => selectUserFromCatalog(state));
 
     console.log(notice);
 
     return (
-        <section id="catalog-section">
+        <section id="catalog-section" className='noticeSection'>
 
             <h1 className="item noticeHeader">
                 <div className="f-right">
                     <Link className="action pad-small f-left" >Delete message</Link>
                 </div>
             </h1>
-
-            <div className="noticeContainer">
-                <div className="notice">
-                    <p className="userName">Message from: <span>{notice.user.firstname}{notice.user.lastName}</span></p>
-                    <p className="userMassege">
-                        <span><i className="fa-regular fa-message"></i></span>
-                        {notice.message}</p>
-                    <form className="noticeForm">
-                        <label>
-                            <textarea name="description" placeholder="write your answer here..."></textarea>
-                        </label>
-                        <div className="align-center">
-                            <input type="submit" value="Response" />
-                        </div>
-                    </form>
+            <div className='noticeSection'>
+                <div className="noticeContainer">
+                    <div className="notice">
+                        <p className="userName">Message from: <span>{notice.user.firstname}{notice.user.lastName}</span></p>
+                        <p className="userMassege">
+                            <span><i className="fa-regular fa-message"></i></span>
+                            {notice.message}</p>
+                        <form className="noticeForm">
+                            <label>
+                                <textarea name="description" placeholder="write your answer here..."></textarea>
+                            </label>
+                            <div className="align-center">
+                                <input type="submit" value="Response" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className='imgContainer'>
+                        <img className="noticeImg" src={notice.product.imgUrl} alt={notice.product.title} />
+                    </div>
                 </div>
-                <div className="noticeImg">
-                    <img src={notice.product.imgUrl} alt={notice.product.title} />
+
+                <div className="item padded">
+                    <footer>
+                        <div>Listed by {currentuser.firstname} </div>
+                    </footer>
+
                 </div>
-
-            </div>
-
-            <div className="item padded">
-                <footer>
-                    <div>Listed by {currentuser.firstname} </div>
-                </footer>
-
             </div>
 
         </section>

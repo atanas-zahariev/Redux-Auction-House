937db90744fe14deeb4fd29a4eb1c40f28e30b18
atanas-zahariev <atanaszaharieff@abv.bg>
2024-05-08 12:12:56 +0300

    Add data on fields.

--- my-app/src/components/common/notice/RecivedNotice.jsx
+++ my-app/src/components/common/notice/RecivedNotice.jsx
@@ -2,11 +2,13 @@
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { selectNoticeById } from '../../../slices/notificationsSlice';
+import { selectUserFromCatalog } from '../../../slices/itemsSlice';
 
 export default function RecivedNotices() {
     const {id} = useParams();
 
     const notice = useSelector(state => selectNoticeById(state,id));
+    const currentuser = useSelector(state => selectUserFromCatalog(state));
 
     console.log(notice);
 
@@ -21,11 +23,10 @@ export default function RecivedNotices() {
 
             <div className="noticeContainer">
                 <div className="notice">
-                    <p className="userName">Message from: <span>Gosho</span></p>
+                    <p className="userName">Message from: <span>{notice.user.firstname}{notice.user.lastName}</span></p>
                     <p className="userMassege">
                         <span><i className="fa-regular fa-message"></i></span>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita fugiat
-                        deleniti ut nesciunt.</p>
+                        {notice.message}</p>
                     <form className="noticeForm">
                         <label>
                             <textarea name="description" placeholder="write your answer here..."></textarea>
@@ -36,14 +37,14 @@ export default function RecivedNotices() {
                     </form>
                 </div>
                 <div className="noticeImg">
-                    <img src="https://thumbs.dreamstime.com/b/brown-horse-isolated-white-7730188.jpg" alt="horse" />
+                    <img src={notice.product.imgUrl} alt={notice.product.title} />
                 </div>
 
             </div>
 
             <div className="item padded">
                 <footer>
-                    <div>Listed by Gosho </div>
+                    <div>Listed by {currentuser.firstname} </div>
                 </footer>
 
             </div>

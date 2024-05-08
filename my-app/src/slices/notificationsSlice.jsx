d8b20f6f3cc4949117e1b6a11ee1b4b76c012a5b
atanas-zahariev <atanaszaharieff@abv.bg>
2024-05-08 07:52:39 +0300

    Get notice functions from api and set initialState

--- my-app/src/slices/notificationsSlice.jsx
+++ my-app/src/slices/notificationsSlice.jsx
@@ -3,3 +3,12 @@ import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/too
 import { makeCorrectIdForRedux, validator } from '../services/utility';
 
 import { api } from '../services/dataService';
+
+const noticeAdapter = createEntityAdapter();
+
+const {createNotification,getAllNotices,getNotice,deleteNotice,editNotice} = api();
+
+const initialState = noticeAdapter.getInitialState({
+    status: 'idle',
+    error: null
+});

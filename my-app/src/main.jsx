1937a0e7d7604f1bdcf95a4e18bfc970daa173ee
Verhus <atanaszaharieff@abv.bg>
2024-06-06 15:23:41 +0300

    Starting query testing.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -11,6 +11,7 @@ import './index.css';
 import { store } from './app/store';
 import { getItems } from './slices/itemsSlice';
 import { getNotifications } from './slices/notificationsSlice';
+
 import { back4appApi } from './services/back4Dataservice';
 
 import Parse from 'parse/dist/parse.min.js';
@@ -19,16 +20,19 @@ Parse.initialize('gyK4yLMJ7Vkdxl10WEuLToXTqtUYiumw8UqPxTmQ', 'Y2Jq1AYuOe08rQbA8r
 
 Parse.serverURL = 'https://parseapi.back4app.com/';
 
-// async function saveNewPerson() {
-//   const person = new Parse.Object('Person');
 
-//   person.set('name', 'John Snow');
-//   person.set('age', 27);
+// async function saveNewPerson() {
+//   const Person = Parse.Object.extend('Person');
+//   const person = new Person();
+//   // const pointer = Person.createWithoutData('hXElJvaZMp');
+//   person.set('name','secondRecord');
+//   person.set('age',36);
+//   person.set('pointer', { '__type': 'Pointer', 'className': '_User', 'objectId': '5dpgWG9xYd' });
 //   try {
-//     let result = await person.save();
-//     alert('New object created with objectId: ' + result.id);
+//     const result = await person.save();
+//     console.log(result);
 //   } catch (error) {
-//     alert('Failed to create new object, with error code: ' + error.message);
+//      console.log(error.message);
 //   }
 // }
 
@@ -39,15 +43,93 @@ Parse.serverURL = 'https://parseapi.back4app.com/';
 //   const query = new Parse.Query('Person');
 
 //   try {
-//     const person = await query.get('mhPFDlCahj');
-//     const name = person.get('name');
-//     const age = person.get('age');
-
-//     alert(`Name: ${name} age: ${age}`);
+//     const person = await query.get('RDro0kMZ6y');
+//     // const name = person.get('name');
+//     // const age = person.get('age');
+//     const { name, age, userPointer} = person.attributes;
+//     console.log(`Name: ${name} age: ${age} pointer ${JSON.stringify(userPointer)}`);
 //   } catch (error) {
 //     alert(`Failed to retrieve the object, with error code: ${error.message}`);
 //   }
 // }
+
+// retrievePerson();
+
+// async function updatePerson() {
+//   const Person = Parse.Object.extend('Person');
+//   const query = new Parse.Query('Person');
+//   try {
+//     const person = await query.get('RDro0kMZ6y');
+//     const pointer = Person.createWithoutData('hXElJvaZMp');
+//     person.set('userPointer', pointer);
+//     await person.save();
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// updatePerson();
+
+// async function removeField(){
+//   const query = new Parse.Query('Person');
+//   try {
+//     const person = await query.get('rOShoyesAz');
+//     person.unset('age');
+//     await person.save();
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// removeField();
+
+// async function deletePerson() {
+//   const query = new Parse.Query('Person');
+
+//   try {
+//     const person = await query.get('rOShoyesAz');
+//     await person.destroy();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// deletePerson();
+
+///// Query -->
+
+async function equalTo(){
+  const Person = Parse.Object.extend('Person');
+  const query = new Parse.Query(Person);
+  query.equalTo('name','firstRecord');
+  try {
+    const result = await query.find();
+    console.log(result);
+    const id = result[0].id;
+    const row = await query.get(id);
+    console.log(row.attributes);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+equalTo();
+
+// async function notEqualTo(){
+//   // const Person = Parse.Object.extend('Person');
+//   // console.log(Person);
+//   const query = new Parse.Query('Person');
+//   query.notEqualTo('name','firstRecord');
+//   try {
+//     const result = await query.find();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// notEqualTo();
+
 
 window.api = back4appApi();
 async function fetchItemBeforeRender() {
